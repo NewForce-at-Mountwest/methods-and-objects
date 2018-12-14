c364f9d3a97ed59e00bbe675bd89acb7dea9e79e
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-12-14 17:49:10 -0500

    added comments for demo code

---
+++ script.js
@@ -0,0 +1,29 @@
+// Build an object representing a lion
+// It should have a name and a food list
+
+// Give this object a method called `roar`
+
+// Invoke the method
+
+// Make another object called antelope and give it a food list
+
+// Give it a method called "leap"
+
+// Invoke the method
+
+
+// Give the lion a method called eat that takes a parameter of "food". It should log "The lion eats the [food]" to the console
+
+
+// Give the antelope a similar eat method
+
+// Have the antelope eat some grass
+
+// Have the lion eat the antelope
+
+
+// Refactor the method so that it loops through the prey list and checks to see if the parameter you passed in is on the prey list. If so, say the lion is eating it. If not, say the lion isn't interested.
+
+// Instead of logging it to the console, return an html string
+
+// Write a function to print the html string to the console

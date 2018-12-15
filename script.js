f31c1b6ec94a0bd5ad4caf24fd300aafcfa4879e
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-12-15 10:54:07 -0500

    added demo code to methods exercise

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
+// lightning exercise review
+
+// Exercise One
+// 1. Create an object that represents a lion
+
+// var lionObject = {
+//     name: "Hank",
+//     foodList: ["antelope", "zebra", "warthogs"],
+//     isCarnivore: true
+// }
+// 2. The object should have the following properties: name (a string), foodList (an array), and isCarnivore (a boolean)
+// Exercise Two
+// 1. Create a function that logs the word "ROARR" to the console.
+// function sayRoar (){
+//     console.log("ROAR");
+// }
+// // 2. Call the function.
+// sayRoar();
+
+var antelope = {
+    name: "Jerry",
+    species: "antelope",
+    foodList: ["grass", "weeds", "flowers"],
+    isCarnivore: false,
+    saySomething: function(){
+        console.log("The antelope is quiet.")
+    }
+}
 // Build an object representing a lion
 // It should have a name and a food list
+var lionObject = {
+    name: "Hank",
+    foodList: ["antelope", "zebra", "warthogs", "Jerry"],
+    isCarnivore: true,
+    sayRoar: function  (){
+        console.log("ROAR");
+    }, 
+    eat: function(foodParameter){
+        var doesHeEatIt;
+        for(var i = 0; i < this.foodList.length; i++){
+            if(foodParameter === this.foodList[i]){
+                doesHeEatIt = `The lion eats the ${foodParameter}`;
+                break;
+            } else {
+                doesHeEatIt = "The lion isn't hungry.";
+            }
+        }
+        console.log(doesHeEatIt);
+    }
+}
+
+console.log(lionObject.name);
+lionObject.sayRoar();
+lionObject.eat("Pizza");
+lionObject.eat(antelope.species);
+
+
 
 // Give this object a method called `roar`
 
@@ -7,9 +62,12 @@
 
 // Make another object called antelope and give it a food list
 
-// Give it a method called "leap"
+
+
 
 // Invoke the method
+antelope.saySomething();
+
 
 
 // Give the lion a method called eat that takes a parameter of "food". It should log "The lion eats the [food]" to the console
